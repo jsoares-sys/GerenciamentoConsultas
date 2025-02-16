8cabaa768214277c50b494c2b86d70b33fe4fa23
jsoares-sys <jjsoares@minha.fag.edu.br>
2025-02-15 21:17:12 -0300

    Adicionando classe médico

---
+++ Medico.java
@@ -0,0 +1,65 @@
+public class Medico {
+  String nome;
+  int idade;
+  int CPF;
+  boolean sexo;
+  String endereco;
+  int telefone;
+
+    public Medico(String nome, int idade, int CPF, boolean sexo, String endereco, int telefone) {
+        this.nome = nome;
+        this.idade = idade;
+        this.CPF = CPF;
+        this.sexo = sexo;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+
+    public boolean isSexo() {
+        return sexo;
+    }
+
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+}
