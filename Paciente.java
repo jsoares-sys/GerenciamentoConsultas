d3b319c410b9913068fbb40712ee1e7ce5bf4a81
jsoares-sys <jjsoares@minha.fag.edu.br>
2025-02-15 21:13:36 -0300

    Adicionando classe paciente

---
+++ Paciente.java
@@ -0,0 +1,68 @@
+public class Paciente {
+
+    String nome;
+    int CPF;
+    int idade;
+    boolean sexo;
+    String endereco;
+    int telefone;
+
+
+    public Paciente(String nome, int CPF, int idade, boolean sexo, String endereco, int telefone) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public boolean isSexo() {
+        return sexo;
+    }
+
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+}
