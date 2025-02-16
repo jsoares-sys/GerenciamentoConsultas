20449df9ef11088990e410fcaf93f89b65725c3a
jsoares-sys <jjsoares@minha.fag.edu.br>
2025-02-16 10:55:33 -0300

    Adicionando classe de consulta

---
+++ Consulta.java
@@ -0,0 +1,45 @@
+public class Consulta {
+   int idConsulta;
+   Date dataConsulta;
+   int horario;
+   String statusConsulta;
+
+   public Consulta(){
+       this.idConsulta = 0;
+       this.dataConsulta = new Date();
+       this.horario = 0;
+       this.statusConsulta = "";
+   }
+
+    public int getIdConsulta() {
+        return idConsulta;
+    }
+
+    public void setIdConsulta(int idConsulta) {
+        this.idConsulta = idConsulta;
+    }
+
+    public Date getDataConsulta() {
+        return dataConsulta;
+    }
+
+    public void setDataConsulta(Date dataConsulta) {
+        this.dataConsulta = dataConsulta;
+    }
+
+    public int getHorario() {
+        return horario;
+    }
+
+    public void setHorario(int horario) {
+        this.horario = horario;
+    }
+
+    public String getStatusConsulta() {
+        return statusConsulta;
+    }
+
+    public void setStatusConsulta(String statusConsulta) {
+        this.statusConsulta = statusConsulta;
+    }
+}
